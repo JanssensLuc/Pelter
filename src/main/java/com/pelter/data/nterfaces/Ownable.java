35f67c715fd03f6fb9b57b095f60d58f5391546d
Janssens <JanssensLuc@users.noreply.github.com>
2016-12-14 14:08:52 +0100

    Delete Ownable.java

--- src/main/java/com/pelter/data/nterfaces/Ownable.java
+++
@@ -1,9 +0,0 @@
-package com.pelter.data.nterfaces;
-
-import com.pelter.data.models.Owner;
-
-public interface Ownable {
-	public boolean hasOwner();
-	public Owner getOwner();
-	public void SetOwner(Owner owner);
-}
